ed0b4ce986a64a68b9dd03c5cc68e88efcc4b287
Saravanan S <selvaraj.saravanan@in.bosch.com>
2021-03-11 21:53:49 +0530

    src/main soruce folder removed because of build pipeline setup

--- vitals/internationalization/Internationalization.java
+++ vitals/internationalization/Internationalization.java
@@ -4,10 +4,10 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Internationalization {
-	  
+
 	public static String getMessage(String key) {
-		Locale.setDefault(Locale.ENGLISH);
-		ResourceBundle bundle = ResourceBundle.getBundle("messages");
+		Locale.setDefault(Locale.GERMAN);
+		ResourceBundle bundle = ResourceBundle.getBundle("vitals/messages/messages");
 		return bundle.getString(key);
 	}
 }
